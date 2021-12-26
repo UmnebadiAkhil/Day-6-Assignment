08adc68d5916e3bdfa2bf8d4048b394f91be050d
Akhil <akhilu5042@gmail.com>
2021-12-26 15:42:42 +0530

    added 03_Program.sh file

---
+++ 03_Program.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+echo -e "Enter Number : \c"
+read n
+
+for ((i=2; i<=$n/2; i++))
+
+do
+	p=$(( n%i ))
+  	if [ $p -eq 0 ]
+  	then
+    		echo "$n is not a prime number."
+		exit 0	
+	fi
+done
+	echo "$n is a prime number."
